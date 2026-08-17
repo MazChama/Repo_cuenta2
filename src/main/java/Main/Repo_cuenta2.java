2052e7ae95c599a6f843e616afec3521c5ed9e3e
MazChama <fmazutiel@gmail.com>
2026-08-16 18:13:18 -0600

    Carpetas

---
+++ src/main/java/Main/Repo_cuenta2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Main;
+
+/**
+ *
+ * @author maria
+ */
+public class Repo_cuenta2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
